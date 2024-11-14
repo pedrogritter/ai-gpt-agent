403aaf2d7e32bd0ab3265d262990b8d044191581
Pedro Gritter <pedro12345g@gmail.com>
2024-11-14 14:47:48 +0000

    feat(agent): :zap: Add agent loop to tool calling

--- src/agent.ts
+++ src/agent.ts
@@ -13,29 +13,35 @@ export const runAgent = async ({
 }) => {
   await addMessages([{ role: "user", content: userMessage }]);
 
-  // const loader = showLoader("Thinking...");
-  console.log("Thinking...");
+  while (true) {
+    console.log("Thinking...");
 
-  const history = await getMessages();
+    const history = await getMessages();
+    const response = await runLLM({
+      messages: history,
+      tools,
+    });
 
-  const response = await runLLM({
-    messages: history,
-    tools: tools,
-  });
+    await addMessages([response]);
 
-  await addMessages([response]);
+    if (response.content) {
+      console.log("Done...");
+      logMessage(response);
 
-  if (response.tool_calls) {
-    const toolCall = response.tool_calls[0];
-    console.log(`Executing tool: ${toolCall.function.name} `);
-    const toolResponse = await runTool(toolCall, userMessage);
-    await saveToolResponse(toolResponse, toolCall.id);
-    console.log(`Tool finished executing: ${toolCall.function.name} `);
+      return getMessages();
+    }
+
+    if (response.tool_calls) {
+      const toolCall = response.tool_calls[0];
+      console.log(`Executing tool: ${toolCall.function.name} `);
+
+      const toolResponse = await runTool(toolCall, userMessage);
+      await saveToolResponse(toolResponse, toolCall.id);
+      console.log(`Tool finished executing: ${toolCall.function.name} `);
+    }
+
+    // console.log("Done...");
+
+    // return getMessages();
   }
-
-  logMessage(response);
-  console.log("Done...");
-  // loader.stop();
-
-  return getMessages();
 };
